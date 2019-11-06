08a688af5735fb8005c1522d4a933c3b5d4062c7
roy <roy.barak.m@gmail.com>
2019-11-06 15:10:15 +0200

    Update AppUserRepo.java

--- restapi/src/main/java/repo/AppUserRepo.java
+++ restapi/src/main/java/repo/AppUserRepo.java
@@ -2,19 +2,13 @@ package repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import io.rainrobot.wake.rest.dto.AppUser;
-
 import java.util.Optional;
 
-@Repository("userDao")
+@Repository
 public interface AppUserRepo extends CrudRepository<AppUser, Integer> {
-	
 	AppUser findByUsername(String username);
-
 	boolean existsByUsername(String username);
-
 	Optional<AppUser> findByEmail(String email);
-
 	Optional<AppUser> findByResetToken(Integer token);
 }

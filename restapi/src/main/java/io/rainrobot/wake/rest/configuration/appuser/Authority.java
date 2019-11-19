0ba763d2e666412c9c02ba466141f1478cc6c2f1
extremeFunk <32679458+extremeFunk@users.noreply.github.com>
2019-11-19 18:07:37 +0200

    Update Authority.java

--- restapi/src/main/java/io/rainrobot/wake/rest/configuration/appuser/Authority.java
+++ restapi/src/main/java/io/rainrobot/wake/rest/configuration/appuser/Authority.java
@@ -1,19 +1,7 @@
 package io.rainrobot.wake.rest.configuration.appuser;
 
 public enum Authority {
-	USER("ROLE_USER"),
-	DBA("ROLE_DBA"),
-	ADMIN("ROLE_ADMIN");
-	
-	String type;
-	
-	private Authority(String type){
-		this.type = type;
-	}
-	
-	public String getType(){
-		return type;
-	}
-	
-	
+	ROLE_USER,
+	ROLE_DBA,
+	ROLE_ADMIN;
 }
